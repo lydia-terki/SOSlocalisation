6c21478d15b4136ffefca01fcf218f2c7e4ab0ce
KaciRayan <moussi.rayan@hotmail.com>
2024-01-18 16:58:29 +0100

    parse patient.csv into event

---
+++ src/main/scala/PatientFileStream.scala
@@ -0,0 +1,38 @@
+
+import GameDates.*
+import SeasonYears.*
+import java.time.LocalDate
+import zio._
+import zio.stream.ZStream
+import com.github.tototoshi.csv._
+
+object PatientFileStream extends ZIOAppDefault {
+
+  override val run: ZIO[Any & ZIOAppArgs & Scope, Throwable, Unit] =
+    for {
+      url <- ZIO.succeed(getClass().getClassLoader().getResource("patient.csv"))
+      source <- ZIO.succeed(CSVReader.open(url.getFile()))
+      events <- ZStream
+        .fromIterator[Seq[String]](source.iterator)
+        .take(10)
+        .map[Option[Event]] {line =>
+            line match {
+              case line if line.head == "date" => None
+              case line =>
+                Some(Event(
+                  date = EventDates.GameDate(LocalDate.parse(line.head)),
+                  lastname = line(1),
+                  firstname = line(2),
+                  age = line(3).toInt,
+                  latitude = line(4).toInt,
+                  longitude = line(5).toInt,
+                  pain = line(6).toInt,
+                  cause = line(7)
+                ))
+            }
+        }
+        .collectSome[Event]
+        .foreach(Console.printLine(_))
+      _ <- ZIO.succeed(source.close())
+    } yield ()
+}
